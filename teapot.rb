2fb7483ace3f0d0b4cfde156d9c9bc7bb7d1c4aa
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-12-08 02:48:09 +1300

    infusion.rb -> teapot.rb

---
+++ teapot.rb
@@ -0,0 +1,30 @@
+
+#
+#  This file is part of the "Teapot" project, and is released under the MIT license.
+#
+
+required_version "0.5"
+
+define_target "png" do |target|
+	target.install do |environment|
+		environment.use in:(package.path + "libpng-1.5.13") do |config|
+			Commands.run("make", "clean") if File.exist? "Makefile"
+				
+			Commands.run("./configure",
+				"--prefix=#{config.install_prefix}",
+				"--disable-dependency-tracking",
+				"--enable-shared=no",
+				"--enable-static=yes",
+				*config.configure
+			)
+				
+			Commands.run("make", "install")
+		end
+	end
+	
+	target.depends :platform
+	
+	target.provides "Library/png" do
+		append linkflags "-lpng"
+	end
+end
