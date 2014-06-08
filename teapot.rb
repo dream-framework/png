266ed24228baa7c63e26e8b351f1cedbe91948fd
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-06-08 18:48:03 +1200

    Trial of updated teapot build system.

--- teapot.rb
+++ teapot.rb
@@ -3,27 +3,35 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "0.8.0"
+teapot_version "1.0.0"
 
 define_target "png" do |target|
-	target.build do |environment|
-		build_external(package.path, "libpng-1.5.13", environment) do |config, fresh|
-			Commands.run("./configure",
-				"--prefix=#{config.install_prefix}",
-				"--disable-dependency-tracking",
-				"--enable-shared=no",
-				"--enable-static=yes",
-				*config.configure
-			) if fresh
-				
-			Commands.make_install
-		end
+	target.build do
+		source_files = Files::Directory.join(target.package.path, "libpng-1.5.13")
+		cache_prefix = Files::Directory.join(environment[:build_prefix], "libpng-1.5.13-#{environment.checksum}")
+		package_files = Path.join(environment[:install_prefix], "lib/pkgconfig/libpng15.pc")
+		
+		cmake source: source_files, build_prefix: cache_prefix
+		
+		make prefix: cache_prefix, package_files: package_files
 	end
 	
 	target.depends :platform
 	target.depends "Library/z"
 	
+	target.depends "Build/Make"
+	target.depends "Build/CMake"
+	
 	target.provides "Library/png" do
 		append linkflags "-lpng"
 	end
+end
+
+define_configuration "png" do |configuration|
+	configuration[:source] = "https://github.com/dream-framework/"
+	
+	configuration.require "platforms"
+	
+	configuration.require "build-make"
+	configuration.require "build-cmake"
 end
