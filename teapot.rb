dfb2dbf8e92df83a7599fbca7d46f2a3368808a8
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-09-25 17:07:05 +1200

    Update libpng and teapot configuration.

--- teapot.rb
+++ teapot.rb
@@ -3,34 +3,34 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "1.0.0"
+teapot_version "3.0.0"
 
 define_target "png" do |target|
-	target.build do
-		source_files = Files::Directory.join(target.package.path, "libpng")
+	target.depends :platform
+	
+	target.depends "Library/z", public: true
+	
+	target.depends "Build/Make"
+	target.depends "Build/CMake"
+	
+	target.provides "Library/png" do
+		source_files = target.package.path + "libpng"
 		cache_prefix = environment[:build_prefix] / environment.checksum + "libpng"
-		package_files = environment[:install_prefix] / "lib/libpng.a"
+		package_files = cache_prefix / "lib/libpng.a"
 		
 		cmake source: source_files, build_prefix: cache_prefix, arguments: [
 			"-DBUILD_SHARED_LIBS=OFF",
 		]
 		
 		make prefix: cache_prefix, package_files: package_files
-	end
-	
-	target.depends :platform
-	target.depends "Library/z"
-	
-	target.depends "Build/Make"
-	target.depends "Build/CMake"
-	
-	target.provides "Library/png" do
-		append linkflags {install_prefix + "lib/libpng.a"}
+		
+		append linkflags package_files
+		append header_search_paths cache_prefix + "include"
 	end
 end
 
 define_configuration "png" do |configuration|
-	configuration[:source] = "https://github.com/dream-framework/"
+	configuration[:source] = "https://github.com/kurocha/"
 	
 	configuration.require "platforms"
 	
